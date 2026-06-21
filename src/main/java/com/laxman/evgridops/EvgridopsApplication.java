0c5d797e2e586d6aa51626e6cd089d6f1ea5ba56
LaxmanSahoo-2004 <laxmanchinmayanandasahoo2004@gmail.com>
2026-06-21 13:10:59 +0530

    Initial spring Boot Project setup

---
+++ src/main/java/com/laxman/evgridops/EvgridopsApplication.java
@@ -0,0 +1,14 @@
+package com.laxman.evgridops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvgridopsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EvgridopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laxman/evgridops/EvgridopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laxman.evgridops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvgridopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
